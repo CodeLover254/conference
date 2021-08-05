c510153ce46e42d90cca130a39b92beb4d8b3bdf
CodeLover254 <astroguypasa@gmail.com>
2021-08-05 11:18:40 +0300

    Added validation annotations

--- src/main/java/com/patrick/conference/model/Registration.java
+++ src/main/java/com/patrick/conference/model/Registration.java
@@ -1,6 +1,14 @@
 package com.patrick.conference.model;
 
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+
 public class Registration {
+
+    @NotNull
+    @NotEmpty
+    @NotBlank
     private String name;
 
     public String getName() {
