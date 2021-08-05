65ab8a47204a3ce1d7f6d1df61f38dcc7345bfe9
CodeLover254 <astroguypasa@gmail.com>
2021-08-05 11:19:41 +0300

    Added validation to the model object

--- src/main/java/com/patrick/conference/controller/RegistrationController.java
+++ src/main/java/com/patrick/conference/controller/RegistrationController.java
@@ -2,9 +2,11 @@ package com.patrick.conference.controller;
 
 import com.patrick.conference.model.Registration;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import javax.validation.Valid;
 
 @Controller
 public class RegistrationController {
@@ -15,7 +17,11 @@ public class RegistrationController {
     }
 
     @PostMapping("registration")
-    public String addRegistration(@ModelAttribute("Registration")Registration registration){
+    public String addRegistration(@Valid @ModelAttribute("Registration")Registration registration, BindingResult result){
+        if(result.hasErrors()){
+            System.out.println("There was an error");
+            return "registration";
+        }
         System.out.println(registration.getName());
         return "redirect:registration";
     }
